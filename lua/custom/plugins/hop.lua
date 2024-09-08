f0d4de5025c360215e41b6840191f0d635b73748
Ernest <ests@users.noreply.github.com>
2024-09-08 18:48:39 +0200

    hop

--- lua/custom/plugins/hop.lua
+++ lua/custom/plugins/hop.lua
@@ -1,6 +1,36 @@
 return {
   'smoka7/hop.nvim',
   version = '*',
+  keys = {
+    {
+      'f',
+      function()
+        require('hop').hint_char1 { direction = require('hop.hint').HintDirection.AFTER_CURSOR, current_line_only = false }
+      end,
+      remap = true,
+    },
+    {
+      'F',
+      function()
+        require('hop').hint_char1 { direction = require('hop.hint').HintDirection.BEFORE_CURSOR, current_line_only = false }
+      end,
+      remap = true,
+    },
+    {
+      't',
+      function()
+        require('hop').hint_char1 { direction = require('hop.hint').HintDirection.AFTER_CURSOR, current_line_only = false, hint_offset = -1 }
+      end,
+      remap = true,
+    },
+    {
+      'T',
+      function()
+        require('hop').hint_char1 { direction = require('hop.hint').HintDirection.BEFORE_CURSOR, current_line_only = false, hint_offset = -1 }
+      end,
+      remap = true,
+    },
+  },
   opts = {
     keys = 'etovxqpdygfblzhckisuran',
   },
