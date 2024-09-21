28014f66a94b9cc7ed509f01cb0d963f58f95ee8
Ernest <ests@users.noreply.github.com>
2024-09-21 22:11:18 +0200

    ToggleTerm

---
+++ lua/custom/plugins/toggleterm.lua
@@ -0,0 +1,19 @@
+return {
+  {
+    'akinsho/toggleterm.nvim',
+    version = '*',
+    cmd = 'ToggleTerm',
+    keys = {
+      { '<C-\\>', '<cmd>ToggleTerm<CR>' },
+    },
+    opts = {
+      size = function(term)
+        if term.direction == 'horizontal' then
+          return 20
+        elseif term.direction == 'vertical' then
+          return vim.o.columns * 0.4
+        end
+      end,
+    },
+  },
+}
