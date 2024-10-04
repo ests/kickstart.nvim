c2e344c4ce017d890e18d00fcbd942baae92052f
Ernest <ests@users.noreply.github.com>
2024-10-04 12:21:37 +0200

    NeoVim welcomes Zig

--- init.lua
+++ init.lua
@@ -566,10 +566,11 @@ require('lazy').setup({
       --  - settings (table): Override the default settings passed when initializing the server.
       --        For example, to see the options for `lua_ls`, you could go to: https://luals.github.io/wiki/settings/
       local servers = {
-        -- clangd = {},
+        clangd = {},
         gopls = {},
-        -- pyright = {},
+        pyright = {},
         rust_analyzer = {},
+        zls = {},
         -- ... etc. See `:help lspconfig-all` for a list of all the pre-configured LSPs
         --
         -- Some languages (like typescript) have entire language plugins that can be useful:
@@ -973,6 +974,7 @@ require('lazy').setup({
         'vimdoc',
         'xml',
         'yaml',
+        'zig',
       },
       incremental_selection = {
         enable = true,
