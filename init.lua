73de05a5719ed6d9582f5e9d04c8d48fa13084cd
Ernest <ests@users.noreply.github.com>
2024-11-21 14:22:07 +0100

    fix: update indent configuration to remove disabled languages

--- init.lua
+++ init.lua
@@ -1004,7 +1004,7 @@ require('lazy').setup({
         --  the list of additional_vim_regex_highlighting and disabled languages for indent.
         additional_vim_regex_highlighting = {},
       },
-      indent = { enable = true, disable = { 'ruby' } },
+      indent = { enable = true, disable = {} },
       endwise = {
         enabled = true,
       },
