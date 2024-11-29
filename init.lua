7be8fe1d1f3ec393caf13029d272a5b83fd22bd7
Ernest <ests@users.noreply.github.com>
2024-11-29 12:57:41 +0100

    feat(lint): add conditional linting for modifiable buffers
    feat(matchup): add vim-matchup plugin with popup navigation
    fix(init): disable additional vim regex highlighting for ruby and remove it from disabled languages

--- init.lua
+++ init.lua
@@ -1011,9 +1011,9 @@ require('lazy').setup({
         --  If you are experiencing weird indenting issues, add the language to
         --  the list of additional_vim_regex_highlighting and disabled languages for indent.
         -- additional_vim_regex_highlighting = { 'ruby' },
-        additional_vim_regex_highlighting = 'ruby',
+        additional_vim_regex_highlighting = false,
       },
-      indent = { enable = true, disable = { 'ruby' } },
+      indent = { enable = true, disable = {} },
       endwise = {
         enabled = true,
       },
