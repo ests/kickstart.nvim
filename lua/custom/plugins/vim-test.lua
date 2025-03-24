a22dfc95667d99f249b390305da4eec827561027
Ernest <ests@users.noreply.github.com>
2025-03-24 13:37:26 +0100

    Need something better than vim-test

--- lua/custom/plugins/vim-test.lua
+++ lua/custom/plugins/vim-test.lua
@@ -8,10 +8,10 @@ return {
     'TestVisit',
   },
   keys = {
-    { '<leader>uu', '<cmd>TestNearest -strategy=wezterm<CR>', desc = '[U]nit nearest', silent = true, noremap = true },
-    { '<leader>un', '<cmd>TestNearest -strategy=toggleterm<CR>', desc = '[U]nit [n]earest (toggleterm)', silent = true, noremap = true },
-    { '<leader>uf', '<cmd>TestFile -strategy=wezterm<CR>', desc = '[U]nit [f]ile', silent = true, noremap = true },
+    { '<leader>uu', '<cmd>TestNearest -strategy=toggleterm<CR>', desc = '[U]nit nearest', silent = true, noremap = true },
+    -- { '<leader>un', '<cmd>TestNearest -strategy=toggleterm<CR>', desc = '[U]nit [n]earest (toggleterm)', silent = true, noremap = true },
+    { '<leader>uf', '<cmd>TestFile -strategy=toggleterm<CR>', desc = '[U]nit [f]ile', silent = true, noremap = true },
     -- { '<leader>us', '<cmd>TestSuite -strategy=wezterm<CR>', desc = '[U]nit [s]suite', silent = true, noremap = true },
-    { '<leader>ul', '<cmd>TestLast -strategy=wezterm<CR>', desc = '[U]nit [l]last', silent = true, noremap = true },
+    { '<leader>ul', '<cmd>TestLast -strategy=toggleterm<CR>', desc = '[U]nit [l]last', silent = true, noremap = true },
   },
 }
