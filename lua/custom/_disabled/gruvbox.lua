83d646899eb7089b387b51add6f81c67183028ea
Ernest <ests@users.noreply.github.com>
2025-03-24 13:37:53 +0100

    move gruvbox to disabled

---
+++ lua/custom/_disabled/gruvbox.lua
@@ -0,0 +1,35 @@
+return {
+  'ellisonleao/gruvbox.nvim',
+  name = 'gruvbox',
+  lazy = false,
+  priority = 1000,
+  config = function()
+    vim.o.background = 'dark' -- or "light" for light mode
+    vim.cmd [[colorscheme gruvbox]]
+    require('gruvbox').setup {
+      terminal_colors = false, -- add neovim terminal colors
+      undercurl = true,
+      underline = true,
+      bold = true,
+      italic = {
+        strings = false,
+        emphasis = true,
+        comments = true,
+        operators = false,
+        folds = true,
+      },
+      strikethrough = true,
+      invert_selection = false,
+      invert_signs = false,
+      invert_tabline = false,
+      invert_intend_guides = false,
+      inverse = true, -- invert background for search, diffs, statuslines and errors
+      contrast = 'soft', -- can be "hard", "soft" or empty string
+      palette_overrides = {},
+      overrides = {},
+      dim_inactive = false,
+      transparent_mode = true,
+    }
+    vim.cmd 'colorscheme gruvbox'
+  end,
+}
