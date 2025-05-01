4907823ddceee2bde0b424249e4d223621f7cbb0
Ernest <ests@users.noreply.github.com>
2025-05-01 23:49:09 +0200

    set claude as default copilot model

--- lua/custom/plugins/codecompanion.lua
+++ lua/custom/plugins/codecompanion.lua
@@ -1,12 +1,24 @@
 return {
   'olimorris/codecompanion.nvim',
-  opts = {},
+  opts = {
+    adapters = {
+      copilot = function()
+        return require('codecompanion.adapters').extend('copilot', {
+          schema = {
+            model = {
+              default = 'claude-3.7-sonnet',
+            },
+          },
+        })
+      end,
+    },
+  },
   dependencies = {
     'nvim-lua/plenary.nvim',
     'nvim-treesitter/nvim-treesitter',
   },
   cmd = {
-    'CdeCompanion',
+    'CodeCompanion',
     'CodeCompanionChat',
     'CodeCompanionAdd',
     'CodeCompanionCmd',
