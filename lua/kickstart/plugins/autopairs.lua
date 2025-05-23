f71bc60f3fb1f89d008c930fc5ef1a9a145ee68a
Ernest <ests@users.noreply.github.com>
2025-05-23 13:35:04 +0200

    remove nvim-cmp autopairs depndency

--- lua/kickstart/plugins/autopairs.lua
+++ lua/kickstart/plugins/autopairs.lua
@@ -4,8 +4,6 @@
 return {
   'windwp/nvim-autopairs',
   event = 'InsertEnter',
-  -- Optional dependency
-  dependencies = { 'hrsh7th/nvim-cmp' },
   config = function()
     local npairs = require 'nvim-autopairs'
     local Rule = require 'nvim-autopairs.rule'
