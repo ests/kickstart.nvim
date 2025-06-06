887ff6f366bcdf81aac97b309e9b76706056116c
Ernest <ests@users.noreply.github.com>
2025-06-06 21:13:53 +0200

    remove clangd and pyright

--- init.lua
+++ init.lua
@@ -644,11 +644,11 @@ require('lazy').setup({
       --  - settings (table): Override the default settings passed when initializing the server.
       --        For example, to see the options for `lua_ls`, you could go to: https://luals.github.io/wiki/settings/
       local servers = {
-        clangd = {},
+        -- clangd = {},
         gopls = {
           -- capabilities = {},
         },
-        pyright = {},
+        -- pyright = {},
         rust_analyzer = {},
         zls = {},
         jsonls = {},
