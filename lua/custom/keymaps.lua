306a017620e406053cada42b9141045641acf64b
Ernest <ests@users.noreply.github.com>
2025-06-17 14:03:56 +0200

    formattin

--- lua/custom/keymaps.lua
+++ lua/custom/keymaps.lua
@@ -2,15 +2,16 @@
 local keymap = vim.keymap.set
 
 -- Map Ctrl+s to save the current buffer
-keymap('n', '<C-s>', ':w<CR>', { noremap = true, silent = true, desc = "Save current buffer" })
-keymap('i', '<C-s>', '<Esc>:w<CR>', { noremap = true, silent = true, desc = "Save current buffer" })
+keymap('n', '<C-s>', ':w<CR>', { noremap = true, silent = true, desc = 'Save current buffer' })
+keymap('i', '<C-s>', '<Esc>:w<CR>', { noremap = true, silent = true, desc = 'Save current buffer' })
+keymap('v', '<C-s>', '<Esc>:w<CR>', { noremap = true, silent = true, desc = 'Save current buffer' })
 
 -- custom basics
 keymap('n', 'Q', '<Nop>', { noremap = true, silent = true })
 keymap('n', 'q', '<Nop>', { noremap = true, silent = true })
 keymap('n', '*', '*<c-o>', { noremap = true, silent = true })
-keymap('n', '<leader>x', '<cmd>Bdelete<cr>', { noremap = true, silent = true, desc = "Close buffer" })
-keymap('n', '<leader>X', '<cmd>bdelete<cr>', { noremap = true, silent = true, desc = "Close buffer (unlisted)" })
+keymap('n', '<leader>x', '<cmd>Bdelete<cr>', { noremap = true, silent = true, desc = 'Close buffer' })
+keymap('n', '<leader>X', '<cmd>bdelete<cr>', { noremap = true, silent = true, desc = 'Close buffer (unlisted)' })
 
 -- save all quit
 local function save_all_and_quit()
@@ -28,24 +29,24 @@ local function save_all_and_quit()
   -- Quit Neovim, closing all windows and discarding any changes in buffers that couldn't be saved
   vim.cmd 'qa!'
 end
-keymap('n', '<c-x>', save_all_and_quit, { noremap = true, silent = true, desc = "Save all and quit" })
+keymap('n', '<c-x>', save_all_and_quit, { noremap = true, silent = true, desc = 'Save all and quit' })
 
 -- movement (window navigation)
-keymap({ 'n', 'v' }, '<c-h>', '<c-w>h', { noremap = true, silent = true, desc = "Move focus to the left window" })
-keymap({ 'n', 'v' }, '<c-j>', '<c-w>j', { noremap = true, silent = true, desc = "Move focus to the lower window" })
-keymap({ 'n', 'v' }, '<c-k>', '<c-w>k', { noremap = true, silent = true, desc = "Move focus to the upper window" })
-keymap({ 'n', 'v' }, '<c-l>', '<c-w>l', { noremap = true, silent = true, desc = "Move focus to the right window" })
+keymap({ 'n', 'v' }, '<c-h>', '<c-w>h', { noremap = true, silent = true, desc = 'Move focus to the left window' })
+keymap({ 'n', 'v' }, '<c-j>', '<c-w>j', { noremap = true, silent = true, desc = 'Move focus to the lower window' })
+keymap({ 'n', 'v' }, '<c-k>', '<c-w>k', { noremap = true, silent = true, desc = 'Move focus to the upper window' })
+keymap({ 'n', 'v' }, '<c-l>', '<c-w>l', { noremap = true, silent = true, desc = 'Move focus to the right window' })
 
 -- toggle last buffers
-keymap('n', ',,', '<cmd>b#<cr>', { noremap = true, silent = true, desc = "Toggle last buffer" })
+keymap('n', ',,', '<cmd>b#<cr>', { noremap = true, silent = true, desc = 'Toggle last buffer' })
 
 -- visual mode move lines
-keymap('v', 'K', ":m '<-2<CR>gv=gv", { noremap = true, desc = "Move line up" })
-keymap('v', 'J', ":m '>+1<CR>gv=gv", { noremap = true, desc = "Move line down" })
+keymap('v', 'K', ":m '<-2<CR>gv=gv", { noremap = true, desc = 'Move line up' })
+keymap('v', 'J', ":m '>+1<CR>gv=gv", { noremap = true, desc = 'Move line down' })
 
 -- visual indent
-keymap('v', '<', '<gv', { noremap = true, silent = true, desc = "Indent left" })
-keymap('v', '>', '>gv', { noremap = true, silent = true, desc = "Indent right" })
+keymap('v', '<', '<gv', { noremap = true, silent = true, desc = 'Indent left' })
+keymap('v', '>', '>gv', { noremap = true, silent = true, desc = 'Indent right' })
 
 -- global abbrevs (commented out)
 -- vim.cmd [[iab <expr> dti strftime("%Y-%m-%d")]]
