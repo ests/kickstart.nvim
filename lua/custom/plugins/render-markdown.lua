93820a6b3e8c84aa47f1512eaf9f359cb2f64bf6
Ernest <ests@users.noreply.github.com>
2025-06-24 19:24:05 +0200

    config: render-markdown

--- lua/custom/plugins/render-markdown.lua
+++ lua/custom/plugins/render-markdown.lua
@@ -1,13 +1,15 @@
 return {
   'MeanderingProgrammer/render-markdown.nvim',
-  cmd = { 'RenderMarkdown' },
-  -- event = 'VeryLazy',
-  -- dependencies = { 'nvim-treesitter/nvim-treesitter', 'echasnovski/mini.nvim' }, -- if you use the mini.nvim suite
-  -- dependencies = { 'nvim-treesitter/nvim-treesitter', 'echasnovski/mini.icons' }, -- if you use standalone mini plugins
-  dependencies = { 'nvim-treesitter/nvim-treesitter', 'nvim-tree/nvim-web-devicons' }, -- if you prefer nvim-web-devicons
+  ft = { 'markdown', 'codecompanion' },
+  -- cmd = { 'RenderMarkdown' }, -- Remove this if using ft
+  dependencies = { 'nvim-treesitter/nvim-treesitter', 'nvim-tree/nvim-web-devicons' },
   ---@module 'render-markdown'
   ---@type render.md.UserConfig
   opts = {
     completions = { blink = { enabled = true } },
+    -- Add more configuration as needed
+    render = {
+      max_file_size = 1.5, -- MB
+    },
   },
 }
