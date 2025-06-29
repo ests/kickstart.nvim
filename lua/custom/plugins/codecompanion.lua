72ccd0376d8df13d74d001e89d43411906f6dbfa
Ernest <ests@users.noreply.github.com>
2025-06-29 11:03:03 +0200

    MCP hub!

--- lua/custom/plugins/codecompanion.lua
+++ lua/custom/plugins/codecompanion.lua
@@ -1,6 +1,16 @@
 return {
   'olimorris/codecompanion.nvim',
   opts = {
+    extensions = {
+      mcphub = {
+        callback = 'mcphub.extensions.codecompanion',
+        opts = {
+          show_result_in_chat = true, -- Show mcp tool results in chat
+          make_vars = true, -- Convert resources to #variables
+          make_slash_commands = true, -- Add prompts as /slash commands
+        },
+      },
+    },
     log_level = 'ERROR',
     send_code = true,
     use_default_actions = true,
