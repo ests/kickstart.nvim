d4de7f6a30c61c699bf02e38be2415fbdff5103b
Ernest <ests@users.noreply.github.com>
2025-07-01 08:06:31 +0200

    config: disable img clip

--- lua/custom/plugins/avante.lua
+++ lua/custom/plugins/avante.lua
@@ -72,19 +72,19 @@ return {
       },
       ft = { 'markdown', 'Avante' },
     },
-    {
-      -- support for image pasting
-      'HakonHarnes/img-clip.nvim',
-      event = 'VeryLazy',
-      opts = {
-        default = {
-          embed_image_as_base64 = false,
-          prompt_for_file_name = false,
-          drag_and_drop = {
-            insert_mode = true,
-          },
-        },
-      },
-    },
+    -- {
+    --   -- support for image pasting
+    --   'HakonHarnes/img-clip.nvim',
+    --   event = 'VeryLazy',
+    --   opts = {
+    --     default = {
+    --       embed_image_as_base64 = false,
+    --       prompt_for_file_name = false,
+    --       drag_and_drop = {
+    --         insert_mode = true,
+    --       },
+    --     },
+    --   },
+    -- },
   },
 }
